58097f3a9e3d70bd5d459c1e404de88fad957ea8
Gabriel Pessi <gabpessi@hotmail.com>
2025-01-23 15:58:37 -0300

    footer-alteracoes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,25 @@ import facebook from '../assets/facebook.svg';
 import linkedin from '../assets/linkedin.svg';
 
 export default function Footer() {
-    const links1 = ["Sobre Nós", "Seja um Franqueado", "Localização das Unidades"];
+    const links1 = [
+        { name: "Sobre Nós", link: "#aboutus" },
+        { name: "Seja um Franqueado", link: "#contact" },
+        { name: "Localização das Unidades", link: "" }
+    ];
+
     const links2 = ["E-mail: contato@must.com.br", "Telefone: (41) 3456-7890", "Endereço: Rua das Araucárias, 1234 - Curitiba, PR"];
+
     const links3 = [
         { name: "Instagram", icon: instagram, link: "https://instagram.com" },
         { name: "Facebook", icon: facebook, link: "https://facebook.com" },
         { name: "LinkedIn", icon: linkedin, link: "https://linkedin.com" }
     ];
-    const links4 = ["Central de Ajuda", "Termos de Uso", "Política de Privacidade"];
+
+    const links4 = [
+        { name: "Central de Ajuda", link: "" },
+        { name: "Termos de Uso", link: "" },
+        { name: "Política de Privacidade", link: "" }
+    ];
 
     return (
         <div className="footer padding-top">
@@ -25,7 +36,9 @@ export default function Footer() {
                 <div>
                     <h4>Links úteis</h4>
                     {links1.map((link, index) => (
-                        <p key={index}>{link}</p>
+                        <a href={link.link} key={index}>
+                            <p>{link.name}</p>
+                        </a>
                     ))}
                 </div>
 
@@ -42,16 +55,21 @@ export default function Footer() {
                     <h4>Redes Sociais</h4>
                     {links3.map((link, index) => (
                         <a href={link.link} key={index} target="_blank" rel="noopener noreferrer">
-                           <div className='social-item'> <img className="icon" src={link.icon} alt={link.name} />
-                            {link.name} </div>
+                            <div className='social-item'>
+                                <img className="icon" src={link.icon} alt={link.name} />
+                                {link.name}
+                            </div>
                         </a>
                     ))}
                 </div>
+                
                 {/* Recursos */}
                 <div>
                     <h4>Recursos</h4>
                     {links4.map((link, index) => (
-                        <p key={index}>{link}</p>
+                        <a href={link.link} key={index}>
+                            <p>{link.name}</p>
+                        </a>
                     ))}
                 </div>
             </div>
